9d1b109d44b1c3a4e7936c4778af4d15ea3b4a4f
Laura Saba <Laura.Saba@ucdenver.edu>
2017-03-31 08:58:31 -0600

    Unknown date

--- code/countRawReads.sh
+++ code/countRawReads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILES1=/projectFolder/rawReads/*.fastq
+FILES1=~/Documents/IntroToRNASeq/rawReads/*.fastq
 for f in $FILES1
 do
-	awk '/@GWZHISEQ/ {getline; print length($0)}' $f | awk -v sample="$f" '{sum+=$1} END {print sample,sum/NR,NR}' >> /projectFolder/data/rawReadCounts.txt
+	awk '/@GWZHISEQ/ {getline; print length($0)}' $f | awk -v sample="$f" '{sum+=$1} END {print sample,sum/NR,NR}' >> ~/Documents/IntroToRNASeq/data/rawReadCounts.txt
 done
